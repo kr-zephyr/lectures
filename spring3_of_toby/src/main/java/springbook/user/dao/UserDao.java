93a37747b83cc4791599e3efc51f756c9074cf1c
SeungChul Lee <Seungchul.lee@nike.com>
2021-03-15 17:39:37 +0900

    extract methods

--- spring3_of_toby/src/main/java/springbook/user/dao/UserDao.java
+++ spring3_of_toby/src/main/java/springbook/user/dao/UserDao.java
@@ -6,9 +6,7 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/springbook?useUnicode=true&characterEncoding=utf8&useSSL=false", "spring", "book");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)");
@@ -23,9 +21,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/springbook?useUnicode=true&characterEncoding=utf8&useSSL=false", "spring", "book");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
@@ -42,6 +38,14 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/springbook?useUnicode=true&characterEncoding=utf8&useSSL=false", "spring", "book");
+
+        return c;
     }
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
